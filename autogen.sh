dd978bd93b940cc7f3547360cc9d9a229275c9d8
Brian Bidulock <bidulock@openss7.org>
2016-02-13 18:11:04 -0700

    minor build system modifications

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ DATE="`date -uI`"
 if [ -z "$VERSION" ]; then
 	VERSION='1.1'
 	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
-		DATE=$(git log --date=iso|grep '^Date:'|head -1|awk '{print$2}')
+		DATE=$(git log --date=iso|grep -m 1 '^Date:'|awk '{print$2}')
 		VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
 		(
 		   echo -e "# created with git log --stat=76 -M -C|fmt -sct -w80\n"
@@ -21,15 +21,19 @@ if [ -z "$VERSION" ]; then
 	fi
 fi
 
-sed -r -e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
-	   s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
-	   s:^DATE=.*$:DATE='$DATE':" \
-	  configure.template >configure.ac
+GTVERSION=`gettext --version|head -1|awk '{print$NF}'|sed -r 's,(^[^\.]*\.[^\.]*\.[^\.]*)\..*$,\1,'`
+
+sed <configure.template >configure.ac -r \
+	-e "s:AC_INIT\([[]$PACKAGE[]],[[][^]]*[]]:AC_INIT([$PACKAGE],[$VERSION]:
+	    s:AC_REVISION\([[][^]]*[]]\):AC_REVISION([$VERSION]):
+	    s:^DATE=.*$:DATE='$DATE':
+	    s:^AM_GNU_GETTEXT_VERSION.*:AM_GNU_GETTEXT_VERSION([$GTVERSION]):"
 
 subst="s:@PACKAGE@:$PACKAGE:g
        s:@VERSION@:$VERSION:g
        s:@DATE@:$DATE:g"
 
+sed -r -e "$subst" README.md.in >README.md
 sed -r -e "$subst" README.in >README
 sed -r -e "$subst" NEWS.in >NEWS
 sed -r -e "$subst" AUTHORS.in >AUTHORS
--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,7 @@
 rm -f cscope.*
 ./autogen.sh
 ./configure.sh
+make clean
 make V=0 cscope
 cscope -b
 make V=0 clean all
